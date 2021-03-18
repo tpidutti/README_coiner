5f4ca51773ddf7b75b33b93fa829ab6659a88349
Theresa Pidutti <treepidutti@gmail.com>
2021-03-18 11:04:43 -0600

    update badges

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+const connectMarkdownFl = require("./generateMarkdown.js");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?",
+    },
+];
+
+inquirer.prompt(questions)
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, ) 
+
+}
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+inquirer.prompt(questions)
+
+}
 
 // Function call to initialize app
 init();
