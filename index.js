c6353f8dcd6a2864db0fd431dba552c896fba3d5
Theresa Pidutti <treepidutti@gmail.com>
2021-03-18 20:07:53 -0600

    additions to readme

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const connectMarkdownFl = require("./generateMarkdown.js");
+const connectMarkdownFl = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -12,31 +12,64 @@ const questions = [
     },
     {
         type: "input",
-        name: "projectTitle",
-        message: "What is the title of your project?",
+        name: "description",
+        message: "How would you describe this project and its purpose?",
     },
     {
         type: "input",
-        name: "projectTitle",
-        message: "What is the title of your project?",
+        name: "installation",
+        message: "This is a demo file so does not need to be installed.",
     },
-];
+    {
+        type: "input",
+        name: "usage",
+        message: "This application can be uses by answering the prompt questions and enjoying the beauty of file creation.",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        choices: ["Apace 2.0", "BSD", "GPL", "MIT", "none"],
+    },
+    {
+        type: "input",
+        name: "contribution",
+        choices: "This project was created by the author and is a demo project.",
+    },
+    {
+        type: "input",
+        name: "tests",
+        choices: "This is a demo project so there are no tests yet.",
+    },
+]
+
+.then({
+    title,
+    description,
+    intallation,
+    usage,
+    license,
+    contribution,
+    tests
+});
 
 
-
-inquirer.prompt(questions)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, ) 
+    fs.writeFile(fileName, data, (err)=>{
+        if(err){
+            console.log(error);
+        }else{
+            console.log("README.md was created.");
+        }
+    }) 
 
 }
 
 
 // TODO: Create a function to initialize app
 function init() {
-inquirer.prompt(questions)
-
+inquirer.prompt(questions);
 }
 
 // Function call to initialize app
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,71 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+const badges = {
+  Apace: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  BSD: "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+  GPL: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+
+}
+function renderLicenseBadge(license) {
+// badges write to top of README file
+
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown({title, description, intallation, usage, license, contribution, tests}) {
+  return `
+  # ${title}
+
+  // badges
+  
+  ## Description
+  ${description}
+
+  ## Table of Contents
+  *[Installation](#installation)
+  *[Usage]
+  *[License]
+  *[Contribution]
+  *[Tests]
+  *[Questions]
+
+  ### Installation
+  ${installation}
+
+  ### Usage
+  ${usage}
+  
+  ### License
+  ${license}
+
+  ### Contribution
+  ${contribution}
+ 
+  ### Tests
+  ${tests}
+
+  ## Questions
+  Please contact me, Theresa, with any questions you might have about this project.  You can reach me at bountravel@hotmail.com or view more of my work at GitHub, [tpidutti](https://github.com/tpidutti).
+
+  `;
+  
+  
+}
+
+console.log(generateMarkdown);
+
+module.exports = generateMarkdown;
+
